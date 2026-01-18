cf6e6b1dfacb06ae6c831fd21768954a7d2acdf7
JOSEEL <herobinekill@gmail.com>
2026-01-17 23:05:22 -0500

    Modify server configuration for deployment
    Updated the server to use environment variable for port and changed host to '0.0.0.0'.

--- server.py
+++ server.py
@@ -56,5 +56,15 @@ async def not_found_handler(request: Request, exc):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    import os
+
+    port = int(os.environ.get("PORT", 8000))  # Render usa PORT automáticamente
+    
+    uvicorn.run(
+        app,
+        host="0.0.0.0",     # Render requiere esto
+        port=port
+    )
+
+
     
